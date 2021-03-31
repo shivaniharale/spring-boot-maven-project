ddf8204d20a7d97c04b9f2662f19d5b505e1115d
Shivani Harale <shivaniharale@gmail.com>
2021-03-31 16:31:17 +0530

    actuator and swagger documentation

--- src/main/java/com/example/employeepayroll/rest/EmployeeController.java
+++ src/main/java/com/example/employeepayroll/rest/EmployeeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.employeepayroll.entity.Details;
@@ -16,6 +17,7 @@ import com.example.employeepayroll.entity.Employee;
 import com.example.employeepayroll.service.ServiceInterface;
 
 @RestController
+@RequestMapping("/api")
 public class EmployeeController {
 	
 	@Autowired
