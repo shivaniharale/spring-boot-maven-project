5c1ac6015afe2b3e3215e86a3d7cab459eafe767
Shivani Harale <shivaniharale@gmail.com>
2021-07-06 13:48:11 +0530

    /logout and /updatepassword and revinfo resolved

--- src/main/java/com/example/employeepayroll/jwt/JwtAuthenticationController.java
+++ src/main/java/com/example/employeepayroll/jwt/JwtAuthenticationController.java
@@ -1,16 +1,22 @@
 package com.example.employeepayroll.jwt;
 
 import com.example.employeepayroll.dto.UserEntityDTO;
+import com.example.employeepayroll.entity.UserEntity;
+import com.example.employeepayroll.repository.UserEntityRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @RestController
 @CrossOrigin
@@ -24,6 +30,12 @@ public class JwtAuthenticationController {
 
     @Autowired
     private JwtUserDetailsService jwtUserDetailsService;
+
+    @Autowired
+    private UserEntityRepo userEntityRepo;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @RequestMapping(value = "/login",method = RequestMethod.POST)
     public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest,
@@ -41,8 +53,33 @@ public class JwtAuthenticationController {
         return ResponseEntity.ok(jwtUserDetailsService.saveUser(userEntityDTO));
     }
 
-    private void authenticate(String userName,String password) throws Exception{
 
+    @PostMapping("/updatepassword")
+    public ResponseEntity<?> changePassword(@RequestParam("oldPassword") String oldPassword,
+                                            @RequestParam("newPassword") String newPassword) throws Exception {
+        authenticate(SecurityContextHolder.getContext().getAuthentication().getName(),oldPassword);
+        UserEntity userEntity= userEntityRepo.findByUserNameEntity(SecurityContextHolder.getContext().getAuthentication().getName());
+
+        userEntity.setPasswordEntity(passwordEncoder.encode(newPassword));
+
+        userEntityRepo.save(userEntity);
+
+        return ResponseEntity.ok("Password updated successfully");
+    }
+
+//    @GetMapping("/logout")
+//    public ResponseEntity<?> logout(HttpServletRequest request){
+//        HttpSession session=request.getSession();
+//        String requestTokenHeader=request.getHeader("Authorization");
+//        if (requestTokenHeader != null && requestTokenHeader.contains("Bearer")) {
+//            session.invalidate();
+//        }
+//        return ResponseEntity.ok("Logout successful");
+//    }
+
+
+
+    private void authenticate(String userName,String password) throws Exception{
         try{
             authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userName,password));
         }catch (DisabledException e){
